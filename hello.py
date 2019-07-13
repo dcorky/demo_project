6bbc5fd3f073fda5bc467ec0bde5e8a47746a451
dcorky <djocorky@gmail.com>
2019-07-13 00:29:45 -0500

    Added Dave response

--- hello.py
+++ hello.py
@@ -4,6 +4,9 @@ def hello():
 def incorrect():
     print('I\'m sorry, that is the wrong input')
 
+def Dave():
+    print('Nah man, Dave\'s not here')
+
 response = input('''Who\'s there?
 ''')
 
@@ -11,3 +14,6 @@ if response == "Hi, it's me.":
     hello()
 else:
     incorrect()
+
+if response == "It's Dave man. I got the stuff.":
+    Dave()
