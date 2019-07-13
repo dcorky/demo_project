90c287fcaa30303bdeeceab5abb225c0b52ea2cd
dcorky <djocorky@gmail.com>
2019-07-13 00:17:30 -0500

    Added input response functionality

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
 def hello():
-    print('Hello!')
+    print('Oh! Hello!')
+
+def incorrect():
+    print('I\'m sorry, that is the wrong input')
+
+response = input('''Who\'s there?
+''')
+
+if input == "Hi, it\'s me.":
+    hello()
+else:
+    incorrect()
